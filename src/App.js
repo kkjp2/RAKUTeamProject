7c421ecfcf2c53a3e80dd8f0b16a2aeab3e5e144
heonjunkim <s01077314486@gmail.com>
2024-09-26 22:47:13 +0900

    시작화면,리스트화면완성

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Layout from './Components/Layout/rental/layout';
+import RentalBoard from './Components/Layout/rental/main/rentalboard';
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import RentalList from './Components/Layout/rental/main/RentalList';
+import RentalWritePage from './Components/Layout/rental/main/rental_writepage';
+import RentalForm from './Components/Layout/rental/main/RentalForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Layout>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<RentalBoard/>}/>    
+    <Route path="/rental_list/:region" element={<RentalList/>}/>
+    <Route path="/rental_write_page" element={<RentalWritePage/>}/>
+    <Route path='/rentalform' element={<RentalForm/>}/>
+      </Routes>
+      </BrowserRouter>
+   </Layout>
+   
   );
 }
 
