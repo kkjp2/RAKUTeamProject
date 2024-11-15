fb2fcd80e9e74fc3c3d6bcdf90b14316c281786d
pcu99 <opcig1999@ync.ac.kr>
2024-11-15 10:10:31 +0900

    Raku_Project

--- src/Components/Layout/realty/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App'; // App 컴포넌트를 import
-import './index.css'; // 기본 스타일 import
-
-const root = ReactDOM.createRoot(document.getElementById('root')); // createRoot로 루트 생성
-root.render(<App />); // App 렌더링
