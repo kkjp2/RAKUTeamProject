6596fb92206516096d794f751c1a897894a5d448
hammersang <142719908+hammersang@users.noreply.github.com>
2024-12-19 01:30:50 +0900

    메인화면 부동산조회 path 연결

--- src/Components/Layout/admin/AdminRouter.js
+++ src/Components/Layout/admin/AdminRouter.js
@@ -11,23 +11,21 @@ import './AdminPage.css';
 
 function App() {
   return (
-    <Router>
       <div className="app_container">
         {/* 左侧导航栏始终保持不变 */}
         <AdminMenu />
         {/* 右侧内容区域根据路由动态变化 */}
         <div className="adminManagement_content">
           <Routes>
-            <Route path="/MoveMain/AdminManagement" element={<AdminManagement />} />
+            <Route path="/admin/AdminManagement" element={<AdminManagement />} />
             {/* <Route path="/MoveMain/AdminCompanyManagement" element={<AdminCompanyManagement />} /> */}
-            <Route path="/MoveMain/AdminRealManagement" element={<AdminRealManagement />} />
-            <Route path='/MoveMain/AdminBoardManaging' element={<AdminBoardManaging/>}/>
-            <Route path='/MoveMain/AdminMembershipManagement' element={<AdminMembershipManagement/>}/>
-            <Route path='/MoveMain/AdminSetting' element={<AdminSetting/>}/>
+            <Route path="/admin/AdminRealManagement" element={<AdminRealManagement />} />
+            <Route path='/admin/AdminBoardManaging' element={<AdminBoardManaging/>}/>
+            <Route path='/admin/AdminMembershipManagement' element={<AdminMembershipManagement/>}/>
+            <Route path='/admin/AdminSetting' element={<AdminSetting/>}/>
           </Routes>
         </div>
       </div>
-    </Router>
   );
 }
 
